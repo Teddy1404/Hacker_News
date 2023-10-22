f20e7b9da5deabd83791ac7826a83dee3c0e947b
Teddy1404 <aryansinghchandel1404@gmail.com>
2023-10-22 17:01:41 +0530

    update url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Search />} />
+        <Route path="/Hacker_News" element={<Search />} />
         <Route path="/post/:postId" element={<PostDetail />} />
       </Routes>
     </BrowserRouter>
